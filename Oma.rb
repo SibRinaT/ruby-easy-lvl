e331eaf6692df75f9caa1bdc5051d9d843e74204
SibRinaT <sibrinat616@gmail.com>
2021-12-17 15:35:23 +0300

    name + Ohm's law

---
+++ Oma.rb
@@ -0,0 +1,8 @@
+print "Enter U: "
+u = gets.to_f
+
+print "Enter R: "
+r = gets.to_f
+
+I = u / r
+puts "result : #{I}" 
---
+++ name.rb
@@ -0,0 +1,8 @@
+print "Enter your name: "
+nam = gets.strip.capitalize
+
+print "Enter surname: "
+sur = gets.strip.capitalize
+
+print "Hello #{nam} #{sur} "
+puts "In your name #{nam.length} letters "
