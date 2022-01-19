7bb167afb6c06eaf0940eb371df870a1f73e395a
SibRinaT <sibrinat616@gmail.com>
2022-01-19 22:06:27 +0300

    easy doing
    On ruby do

---
+++ times1.rb
@@ -0,0 +1,8 @@
+x = 0
+   2.times do 
+    500.times do
+      x = x + 1 
+     end 
+   end
+print x
+ 
---
+++ times2.rb
@@ -0,0 +1,10 @@
+10.times do |x|
+ print "Hello "
+
+      x.times do
+    print "!" 
+         end 
+ puts 
+end
+ 
+
