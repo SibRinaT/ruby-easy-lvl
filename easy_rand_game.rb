d834942a2f3c616fece8044dd2645738e1462631
SibRinaT <sibrinat616@gmail.com>
2022-01-26 22:14:27 +0300

    Update easy_rand_game.rb

--- easy_rand_game.rb
+++ easy_rand_game.rb
@@ -1,13 +1,16 @@
 puts "how much to play? : "
 a = gets.to_i
 
+puts "Enter your favorite number: "
+f = gets.to_i
+
 1.upto(a) do |nn| 
 	#puts "Please press enter: "
 #gets 
        puts "Play on #{nn}"
 	x = rand(1..50)
 
-	if x == 33
+	if x == f
 		print "YOU WIN! "
  	else 
             print "lose! "
