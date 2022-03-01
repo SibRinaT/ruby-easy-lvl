c7fce9f34c7a2e5dac73bb48caf4517296f8c520
ильдар <36659596+senseiseo@users.noreply.github.com>
2022-03-01 08:28:23 +0300

    Update array3.1_dellete.rb

--- array/array3.1_dellete.rb
+++ array/array3.1_dellete.rb
@@ -13,3 +13,49 @@ loop do
 
 	arr.delete_at n - 1
 end  
+arr = %w[]
+
+loop do 
+  arr2 = []
+
+  x = 0 
+  arr.each do |person|
+    x = x + 1
+    puts "#{x}. #{person[0]} #{person[1]}"
+  end 
+
+  puts "Who you want to add : "
+  people = gets.chomp
+  arr2 << people
+
+  puts "Enter age: "
+  age = gets.to_i 
+  arr2 << age
+
+  arr << arr2
+
+
+  puts "You want delete a name or age on list ?"
+  ans1 = gets.capitalize.strip
+
+  if ans1 == "Y"
+    puts "What are u want a delete? Name(1) or age(2) ? (1/2)"
+    ans2 = gets.to_i
+    if ans2 == 1
+      puts "When name u want a delete? "
+      delName = gets.to_i
+      arr.delete_at delName - 1
+    end
+
+  elseif ans2 == 2
+    puts "When age u want a delete? "
+    delAge = gets.to_i
+
+    arr.delete_at delAge - 1
+  end
+
+  if people == "" || age == ""
+    break 
+  end  
+end
+#ОЦЕНИ КОД !
