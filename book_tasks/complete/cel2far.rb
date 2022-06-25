515ab04fc90f005cf1dd1c7abe63c46aff3c9fd6
Ainur <86946469+SibRinaT@users.noreply.github.com>
2022-06-25 23:31:03 +0300

    ez_cel_far

---
+++ book_tasks/complete/cel2far.rb
@@ -0,0 +1,10 @@
+def cel2far(cel:)
+    puts cel * 9/5 + 32          #1far = C * 9/5 + 32 
+end
+cel2far(cel:1) 
+
+def fal2cel(far:)
+   puts 5/9*(far - 32)           #1cel = 5/9(F-32)
+end 
+fal2cel(far:1)
+
